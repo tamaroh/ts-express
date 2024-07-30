f68993e0ef36b31a12da452b55faf76de1920fa1
Tamaroh <tamaroh@gmail.com>
2024-07-30 11:58:34 +0900

    prisma test

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-console.log("Hello");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .then(async () => {
+    console.log("it worked");
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ readme.md
@@ -0,0 +1,7 @@
+```
+npm i
+```
+create database
+create .env , edit (reference: .env.sample)
+npx prisma migrate dev --name init
+```
